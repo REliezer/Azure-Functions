f2107037d6654bd38d766682ad8f44629e8f66f1
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-03-08 09:33:13 -0600

    fix(postInscriptionActivity)

--- src/functions/Actividades/InscripcionActividades/postInscriptionActivity.ts
+++ src/functions/Actividades/InscripcionActividades/postInscriptionActivity.ts
@@ -8,7 +8,7 @@ export async function postInscriptionActivity(request: HttpRequest, context: Inv
 
         const body = await request.json() as {
             actividad_id: string;
-            becario_id: string
+            no_cuenta: string
         };
 
         let pool = await getDbConnection();
@@ -17,29 +17,26 @@ export async function postInscriptionActivity(request: HttpRequest, context: Inv
         // Ejecutar el procedimiento almacenado
         let result = await pool.request()
             .input("actividad_id", sql.VarChar, body.actividad_id)
-            .input("becario_id", sql.NChar, body.becario_id)
+            .input("no_cuenta", sql.NVarChar, body.no_cuenta)
             .execute("insert_nueva_inscripcion_act");  // Llamamos al procedimiento almacenado
 
         if (result.rowsAffected[0] === 0) {
             return { status: 404, body: "Fallo la inscripción en la actividad." };
         }
-        let responseData = {
-            inscripcion: result.recordset
-        };
 
-        return { status: 200, body: "Inscripcion a la actividad exitosamente." };
+        return { status: 200, body: JSON.stringify({ message: "Inscripcion a la actividad exitosamente." })};
     } catch (error) {        
-        if (error instanceof sql.RequestError && error.number === 50000) {
+        if (error instanceof sql.RequestError && (error.number === 50000 || error.number === 50001)) {
             context.log(`Error personalizado: ${error.message}`);
             return {
                 status: 400,
-                body: "Ya estás inscripto(a) en esta actividad!",
+                body: JSON.stringify({ error: error.message })
             };
         } else {
             context.log(`Error: ${error}`);
             return {
                 status: 500,
-                body: 'Internal Server Error'
+                body: JSON.stringify({ error: 'Internal Server Error' })
             };
         }
     }
