21998d40d6d993ad1bd156f0e72fd6d89ed18ea3
Rodrigo Fúnes <91682387+REliezer@users.noreply.github.com>
2025-04-03 11:04:27 -0600

    (fix): getBecarioActivity

--- src/functions/Actividades/getBecarioActivity.ts
+++ src/functions/Actividades/getBecarioActivity.ts
@@ -1,16 +1,16 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { getDbConnection } from "../dbConnection";
+import * as sql from "mssql";
 
 export async function getBecarioActivity(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    try {
-        
+    try {        
         context.log(`Http function processed request for url "${request.url}"`);
         
         const body = await request.json() as {
             centro_id: number;            
         };
 
-        if (!body.centro_id ) {
+        if (body.centro_id === undefined || body.centro_id === null) {
             return {
                 status: 400,
                 body: JSON.stringify("Falta el parámetro 'centro_id'.")
@@ -21,6 +21,7 @@ export async function getBecarioActivity(request: HttpRequest, context: Invocati
         context.log("Connected to database");
 
         let result = await pool.request()
+            .input("centro_id", sql.Int, body.centro_id)
             .execute("getBecarioActivities");
         context.log("Consulta ejecutada con éxito");
 
